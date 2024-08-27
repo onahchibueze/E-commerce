aec703488bbb7cbec2c1f99c69c021c4616bdf52
Onahchibueze <chibuezesomotochukwu09@gmail.com>
2024-08-27 11:13:32 +0100

    Describe the changes you made

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -3,7 +3,7 @@ import './CSS/ShopCategory.css'
 import { ShopContext } from '../context/ShopContext'
 import dropdown_icon from '../components/assets/dropdown_icon.png'
 import { Item } from '../components/Item/Item'
-import all_product from '../components/assets/all_product'
+
 export const ShopCategory = (props) => {
     const { all_product } = useContext(ShopContext);
     return (
